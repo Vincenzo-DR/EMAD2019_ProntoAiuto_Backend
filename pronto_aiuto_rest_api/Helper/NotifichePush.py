161419bb239aeeedaa005c8852889f33839d284c
Vincenzo-DR <57796878+Vincenzo-DR@users.noreply.github.com>
2020-02-17 18:27:40 +0100

    fix invia notifica per richiesta supporto dalla homepage

--- pronto_aiuto_rest_api/Helper/NotifichePush.py
+++ pronto_aiuto_rest_api/Helper/NotifichePush.py
@@ -14,14 +14,14 @@ def sendNotificaToFO(playerId, pk_request, tipo_request, richiesta_from):
                    "headings": {"en": "Nuova richiesta di soccorso"},
                    "contents": {"en": "Una voltante ha bisogno del tuo aiuto."},
                    "data": {"req_pk": pk_request, "richiesta_from": richiesta_from,
-                            "lat": richiesta.lat, "long": richiesta.long}
+                            "lat": richiesta.lat, "long": int(richiesta.long)}
                    }
     else:
         payload = {"app_id": "a25229e0-e3d2-419c-8706-8c0abbe60353",
                    "include_player_ids": [playerId],
                    "headings": {"en": "Nuova richiesta di soccorso"},
                    "contents": {"en": "Emergenza per {}.".format(tipo_request)},
-                   "data": {"req_pk": pk_request, "richiesta_from": richiesta_from}
+                   "data": {"req_pk": pk_request, "richiesta_from": int(richiesta_from)}
                    }
 
     return requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
